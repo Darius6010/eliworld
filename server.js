b618436c8aaeb074ccd187474542d7f5b7222abe
Darius6010 <anutmariana15@gmail.com>
2026-01-10 13:34:02 +0000

    Initial server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+res.send("Senworld server is running!");
+});
+
+io.on("connection",(socket) => {
+console.log("Player disconnected:", socket.id);
+ });
+
+server.listen(3000, () => {
+console.log("Server is running on http://localhost:3000");
+});
