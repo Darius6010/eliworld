c4dd2f44c0d780e132fe57053ab11316c27710aa
Darius6010 <anutmariana15@gmail.com>
2026-01-10 13:40:17 +0000

    server.js

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ io.on("connection",(socket) => {
 console.log("Player disconnected:", socket.id);
  });
 
-server.listen(3000, () => {
-console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT|| 3000;
+server.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+
 });
