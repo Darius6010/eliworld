db93799b3463f69ebaa5774ea48aa2467f231477
Darius6010 <anutmariana15@gmail.com>
2026-01-10 12:37:45 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const {Server} = require("Socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("../client"));
+
+io.on("connection", (socket) => {
+    console.log("Player connected:", socket.id);
+
+    socket.on("move", data => {
+        socket.broadcast.emit("move", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Player disconnected:",
+             socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
